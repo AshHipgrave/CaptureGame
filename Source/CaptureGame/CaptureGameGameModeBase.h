c99ad8a679564acda929bea1f777bf50de4a2793
Ash Hipgrave <ahipgrave@live.co.uk>
2018-03-05 19:33:26 +0000

    Adding project files

---
+++ Source/CaptureGame/CaptureGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CaptureGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPTUREGAME_API ACaptureGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
